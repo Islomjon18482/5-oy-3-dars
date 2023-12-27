2b65ee73fa54ab6c392acda7a7f84a478d540c75
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2023-12-27 20:42:59 +0500

    js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const card1 = document.querySelector(".card1")
 const cards = document.getElementsByClassName("another__card")
 let n = Math.trunc(Math.random(0, 1)*3)
 btn.addEventListener("click", function(){
-    // card1.style.display = "none"
+    card1.style.display = "none"
     cards[n].style.display = "block"
 })
